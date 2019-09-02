ae59f641c135c61dceb361234f4e99a8d11cc536
Greg Herbowicz <herbowicz@gmail.com>
2019-09-02 22:40:30 +0200

    dirname

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 const db = require("./db");
 const movieRouter = require("./routes/movie-router");
@@ -8,20 +9,25 @@ const movieRouter = require("./routes/movie-router");
 const app = express();
 const apiPort = 3000;
 
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
 app.get("/test-api", (req, res) => {
   res.send("Test http://80.211.3.196:3000/test-api");
 });
 
-app.use(__dirname + "/api", movieRouter);
+app.use("/api", movieRouter);
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
