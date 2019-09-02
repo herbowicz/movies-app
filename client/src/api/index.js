47800c8360a94b774f9da127625688389337a892
Greg Herbowicz <herbowicz@gmail.com>
2019-09-02 19:52:53 +0200

    new order

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,19 +4,19 @@ const api = axios.create({
   baseURL: "http://80.211.3.196:3000/api"
 });
 
-export const insertMovie = payload => api.post(`/movie`, payload);
 export const getAllMovies = () => api.get(`/movies`);
+export const getMovieById = id => api.get(`/movie/${id}`);
+export const insertMovie = payload => api.post(`/movie`, payload);
 export const updateMovieById = (id, payload) =>
   api.put(`/movie/${id}`, payload);
 export const deleteMovieById = id => api.delete(`/movie/${id}`);
-export const getMovieById = id => api.get(`/movie/${id}`);
 
 const apis = {
-  insertMovie,
   getAllMovies,
+  getMovieById,
+  insertMovie,
   updateMovieById,
-  deleteMovieById,
-  getMovieById
+  deleteMovieById
 };
 
 export default apis;
--- server/routes/movie-router.js
+++ server/routes/movie-router.js
@@ -4,10 +4,10 @@ const MovieCtrl = require('../controllers/movie-ctrl')
 
 const router = express.Router()
 
+router.get('/movies', MovieCtrl.getMovies)
+router.get('/movie/:id', MovieCtrl.getMovieById)
 router.post('/movie', MovieCtrl.createMovie)
 router.put('/movie/:id', MovieCtrl.updateMovie)
 router.delete('/movie/:id', MovieCtrl.deleteMovie)
-router.get('/movie/:id', MovieCtrl.getMovieById)
-router.get('/movies', MovieCtrl.getMovies)
 
 module.exports = router
