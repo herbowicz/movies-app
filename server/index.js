c7a103d453b0717339d9ce46afc8e70fc575f253
Greg Herbowicz <herbowicz@gmail.com>
2019-09-02 22:02:47 +0200

    use api

--- server/index.js
+++ server/index.js
@@ -1,27 +1,27 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const db = require('./db')
-const movieRouter = require('./routes/movie-router')
+const db = require("./db");
+const movieRouter = require("./routes/movie-router");
 
-const app = express()
-const apiPort = 3000
+const app = express();
+const apiPort = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/test-api', (req, res) => {
-    res.send('Test http://80.211.3.196:3000/test-api')
-})
+app.get("/test-api", (req, res) => {
+  res.send("Test http://80.211.3.196:3000/test-api");
+});
 
-app.use('/movies-app/api', movieRouter)
+app.use("/api", movieRouter);
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
